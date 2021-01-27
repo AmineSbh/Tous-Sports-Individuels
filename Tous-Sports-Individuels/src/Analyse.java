00e17ef4539347a0f4e3a8eff1ea61bee191fc43
Amine Sabbahi <amine.sabbahi@gmail.com>
2021-01-27 20:10:46 +0100

    Analyse

--- Tous-Sports-Individuels/src/Analyse.java
+++ Tous-Sports-Individuels/src/Analyse.java
@@ -51,47 +51,24 @@ public class Analyse {
 		frmPageAnalyse = new JFrame();
 		frmPageAnalyse.getContentPane().setBackground(Color.DARK_GRAY);
 		frmPageAnalyse.setTitle("Page Analyse");
-		frmPageAnalyse.setBounds(100, 100, 791, 585);
+		frmPageAnalyse.setBounds(100, 100, 749, 531);
 		frmPageAnalyse.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmPageAnalyse.getContentPane().setLayout(null);
 		
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setToolTipText("");
-		comboBox_1.setBounds(499, 76, 159, 26);
+		comboBox_1.setBounds(36, 198, 159, 26);
 		comboBox_1.addItem(comboBox_1.getSelectedItem());
 		frmPageAnalyse.getContentPane().add(comboBox_1);
 		
-		Panel panel = new Panel();
-		panel.setBounds(26, 114, 447, 405);
-		frmPageAnalyse.getContentPane().add(panel);
-		panel.setLayout(null);
-		
-		JLabel lblMoyenneParJour = new JLabel("Moyenne \r\n/ jour");
-		lblMoyenneParJour.setForeground(Color.WHITE);
-		lblMoyenneParJour.setBounds(330, 34, 122, 37);
-		panel.add(lblMoyenneParJour);
-		
-		JLabel label = new JLabel("Moyenne \r\n/ jour");
-		label.setForeground(Color.WHITE);
-		label.setBounds(330, 161, 122, 37);
-		panel.add(label);
-		
-		JButton btnValider = new JButton("Valider");
-		btnValider.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnValider.setBounds(499, 387, 159, 29);
-		frmPageAnalyse.getContentPane().add(btnValider);
-		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setToolTipText("");
-		comboBox.setBounds(499, 246, 159, 26);
+		comboBox.setBounds(353, 198, 159, 26);
 		frmPageAnalyse.getContentPane().add(comboBox);
 		
 		JButton btnRetour_Menu = new JButton("Retour au menu principal");
 		btnRetour_Menu.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		btnRetour_Menu.setBounds(593, 461, 161, 38);
+		btnRetour_Menu.setBounds(531, 423, 161, 38);
 		frmPageAnalyse.getContentPane().add(btnRetour_Menu);
 		
 		JLabel label_1 = new JLabel("Tous Sport, le sport pour tous");
@@ -108,14 +85,26 @@ public class Analyse {
 		
 		JLabel lblChoixDuSport = new JLabel("Choix du sport \u00E0 analyser");
 		lblChoixDuSport.setForeground(Color.WHITE);
-		lblChoixDuSport.setBounds(499, 50, 180, 20);
+		lblChoixDuSport.setBounds(36, 152, 180, 20);
 		frmPageAnalyse.getContentPane().add(lblChoixDuSport);
 		lblChoixDuSport.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		
 		JLabel lblChoixDunAmi = new JLabel("Choix d'un ami pour comparaison");
 		lblChoixDunAmi.setForeground(Color.WHITE);
-		lblChoixDunAmi.setBounds(499, 220, 239, 20);
+		lblChoixDunAmi.setBounds(353, 152, 239, 20);
 		frmPageAnalyse.getContentPane().add(lblChoixDunAmi);
 		lblChoixDunAmi.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JButton btnDonnes = new JButton("Donn\u00E9es 1");
+		btnDonnes.setBounds(26, 319, 178, 29);
+		frmPageAnalyse.getContentPane().add(btnDonnes);
+		
+		JButton btnDonnes_1 = new JButton("Donn\u00E9es 2");
+		btnDonnes_1.setBounds(261, 319, 191, 29);
+		frmPageAnalyse.getContentPane().add(btnDonnes_1);
+		
+		JButton btnDonnes_2 = new JButton("Donn\u00E9es 3");
+		btnDonnes_2.setBounds(490, 319, 222, 29);
+		frmPageAnalyse.getContentPane().add(btnDonnes_2);
 	}
 }
