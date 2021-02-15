049517f4a009f03b3fefcd0c30ed8a225f2ec6d8
Amine Sabbahi <amine.sabbahi@gmail.com>
2021-02-15 17:10:20 +0100

    addDataVisitor

---
+++ Tous-Sports-Individuels/src/process/AddDataVisitor.java
@@ -0,0 +1,41 @@
+package process;
+
+import data.Course;
+import data.Cyclisme;
+import data.Football;
+import data.Natation;
+import data.Tennis;
+
+public class AddDataVisitor implements SportVisitor<Void>{
+
+	@Override
+	public Void visit(Course sport) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visit(Cyclisme sport) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visit(Football sport) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visit(Natation sport) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visit(Tennis sport) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
